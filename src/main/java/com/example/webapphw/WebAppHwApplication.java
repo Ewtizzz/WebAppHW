b5d60abbc33bdb137f76857d020fffe5baa577bf
Ewtizzz <egor.zholobov@gmail.com>
2023-02-10 17:48:11 +0300

    initial commit

---
+++ src/main/java/com/example/webapphw/WebAppHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.webapphw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webapphw/WebAppHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapphw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
